befb2fb3c307a91f0a96c1fc4ec3a32cb0fab1a6
Marin-Marie.Le-Cornec <marin-marie.le-cornec@insa-rennes.fr>
2022-03-14 14:22:41 +0100

    adding some changes on error handling

--- ActionCompiler/Compiler/SemErrorSectionOffMapVisitor.cs
+++ ActionCompiler/Compiler/SemErrorSectionOffMapVisitor.cs
@@ -9,8 +9,9 @@ namespace Action.Compiler
     public class SemErrorSectionOffMapVisitor : NodeVisitor<IEnumerable<DiagnosticResult>>
     {
         public IEnumerable<DiagnosticResult> Visit(List<ComplexNode> nodes)
-        {
-            foreach (var node in nodes)
+         {
+            IEnumerable<MapNode> query1 = nodes.OfType<MapNode>();
+            foreach (var node in query1)
             {
                 foreach (var symbol in Visit(node))
                     yield return symbol;
@@ -18,38 +19,44 @@ namespace Action.Compiler
         }
 
         public override IEnumerable<DiagnosticResult> VisitMap(MapNode mapNode){
-            GetCoord(mapNode);
-            return CheckLimit(mapNode);
-        }
-
-        public override IEnumerable<DiagnosticResult> VisitSection(SectionNode sectionNode)
-        {
-            return CheckLimit(sectionNode);
-        }
-
-        public IEnumerable<PropertyNode> GetCoord(ComplexNode node){
-            foreach(var property in node.properties){
-                if(property.identifier is ShapeKeywordNode){
-                    ComplexNode val = (ComplexNode)property.value;
-                    if(val.type is BoxKeywordNode){
-                        yield break;
+            var size = GetSize(mapNode);
+            foreach(var sec in mapNode.sections){
+                if(sec is ReferenceNode valRef){
+                    if(valRef.coords.x.integer > size[0] && valRef.coords.y.integer > size[1]){
+                        yield return new DiagnosticResult(Severity.Error, "section is off the map");
                     }
                 }
-            }
-        }
-
-        public IEnumerable<DiagnosticResult> CheckLimit(ComplexNode node){
-
-            foreach(var property in node.properties){
-                if(property.identifier is ShapeKeywordNode){
-                    ComplexNode val = (ComplexNode)property.value;
-                    if(val.type is BoxKeywordNode){
-                        if(!val.properties.Any()){
-                            yield return new DiagnosticResult(Severity.Error, "missing height and/or width of the box");
+                if(sec is SectionNode valSec){
+                    if(valSec.coords != null){
+                        if(valSec.coords.x.integer > size[0] && valSec.coords.y.integer > size[1]){
+                            yield return new DiagnosticResult(Severity.Error, "section is off the map");
                         }
+                    } 
+                }   
+            }
+        }
+
+        public int[] GetSize(ComplexNode node){
+            int[] size = new int[] {0,0};
+            foreach(var property in node.properties){
+                if(property.identifier is ShapeKeywordNode){
+                    ComplexNode val = (ComplexNode)property.value;
+                    if(val.type is BoxKeywordNode){
+                        foreach(var prop in val.properties){
+                            if(prop.identifier is HeightKeywordNode){
+                                IntNode res = (IntNode)prop.value;
+                                size[0] = res.integer;
+                            }
+                            if(prop.identifier is WidthKeywordNode){
+                                IntNode res = (IntNode)prop.value;
+                                size[1] = res.integer;
+                            }
+                        } 
                     }
                 }
             }
+            return size;
         }
+
     }
 }
