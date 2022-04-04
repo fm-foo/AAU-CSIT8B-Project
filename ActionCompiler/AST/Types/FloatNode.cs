c00d0bfe832aaa0c6d4ef555128b948809278e78
Péter Sebők <33090588+sebokpeter@users.noreply.github.com>
2022-04-04 13:53:08 +0200

    Semantic error visitors moved to folder

---
+++ ActionCompiler/AST/Types/FloatNode.cs
@@ -0,0 +1,12 @@
+﻿namespace Action.AST
+{
+
+    // TODO: save value as a stirng (to enable error checking) and have a property to convert to float
+    public record FloatNode(float f) : ValueNode
+    {
+        public override T Accept<T>(NodeVisitor<T> visitor)
+        {
+            return visitor.VisitFloat(this);
+        }
+    }
+}
--- ActionCompiler/AST/Types/IntNode.cs
+++ ActionCompiler/AST/Types/IntNode.cs
@@ -7,12 +7,4 @@ namespace Action.AST
             return visitor.VisitInt(this);
         }
     }
-
-    public record FloatNode(float f) : ValueNode
-    {
-        public override T Accept<T>(NodeVisitor<T> visitor)
-        {
-            return visitor.VisitFloat(this);
-        }
-    }
 }
